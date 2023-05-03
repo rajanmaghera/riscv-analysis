a77225b29db8c4b784d6b6d5555148afb139d775
Rajan Maghera <rmaghera@ualberta.ca>
2023-05-03 13:25:42 -0600

    Construct lexer

---
+++ src/parser/lexer.rs
@@ -0,0 +1,149 @@
+use crate::parser::token::{Position, Range, TokenInfo};
+use crate::{SymbolData, Token};
+
+const EOF_CONST: char = 3 as char;
+
+pub struct Lexer {
+    // TODO use bytes (u8array) for everything here
+    // TODO switch to iter methods on bytes
+    source: String,
+    ch: char,
+    pos: usize,
+    row: usize,
+    col: usize,
+}
+
+impl Lexer {
+    pub fn new<S: Into<String>>(source: S) -> Lexer {
+        let mut lex = Lexer {
+            source: source.into(),
+            ch: '\0',
+            pos: 0,
+            row: 0,
+            col: 0,
+        };
+        lex.next_char();
+        lex
+    }
+
+    pub fn tokenize<S: Into<String>>(input: S) -> Vec<TokenInfo> {
+        Lexer::new(input).collect()
+    }
+
+    fn next_char(&mut self) {
+        let b = self.source.as_bytes();
+
+        if self.pos >= self.source.len() {
+            self.ch = EOF_CONST;
+        } else {
+            self.ch = b[self.pos] as char;
+        }
+
+        if self.ch == '\n' {
+            self.row += 1;
+            self.col = 0;
+        } else {
+            self.col += 1;
+        }
+
+        self.pos += 1;
+    }
+
+    fn is_space(&self) -> bool {
+        self.ch == ' ' || self.ch == '\t' || self.ch == '\n'
+    }
+
+    fn is_symbol_char(&self) -> bool {
+        let c = self.ch;
+        (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_'
+    }
+
+    fn is_symbol_item(&self) -> bool {
+        let c = self.ch;
+        self.is_symbol_char() || (c >= '0' && c <= '9')
+    }
+
+    fn skip_ws(&mut self) {
+        while self.is_space() {
+            self.next_char();
+        }
+    }
+}
+
+// TODO switch to tokeninfo struct
+
+impl Iterator for Lexer {
+    type Item = TokenInfo;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.skip_ws();
+        let token = match self.ch {
+            ':' => Some(TokenInfo {
+                token: Token::Colon,
+                pos: Range {
+                    start: Position {
+                        line: self.row,
+                        column: self.col,
+                    },
+                    end: Position {
+                        line: self.row,
+                        column: self.col,
+                    },
+                },
+            }),
+            ',' => Some(TokenInfo {
+                token: Token::Comma,
+                pos: Range {
+                    start: Position {
+                        line: self.row,
+                        column: self.col,
+                    },
+                    end: Position {
+                        line: self.row,
+                        column: self.col,
+                    },
+                },
+            }),
+            '.' => todo!("directives"),
+            '#' => todo!("comments"),
+            _ => {
+                let start = Position {
+                    line: self.row,
+                    column: self.col,
+                };
+
+                let mut symbol_str: String = "".to_owned();
+
+                while self.is_symbol_item() {
+                    symbol_str += &self.ch.to_string();
+                    self.next_char();
+                }
+
+                if symbol_str == "" {
+                    return None;
+                } else if self.ch == ':' {
+                    // this is a label
+                    self.next_char();
+                    let end = Position {
+                        line: self.row,
+                        column: self.col,
+                    };
+                    return Some(TokenInfo {
+                        token: Token::Label(symbol_str.to_owned()),
+                        pos: Range { start, end },
+                    });
+                }
+                Some(TokenInfo {
+                    token: Token::Symbol(SymbolData(symbol_str.to_owned())),
+                    pos: Range { start, end: start },
+                })
+            }
+        };
+
+        if let Some(_) = token {
+            self.next_char();
+        }
+
+        token
+    }
+}
