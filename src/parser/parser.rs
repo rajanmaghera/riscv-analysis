f0fe599f4c00d132369b3f1493f11c8cb1dff14f
Rajan Maghera <rmaghera@ualberta.ca>
2023-05-08 15:24:18 -0600

    Add error detection to ASTNode iterator

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -2,6 +2,9 @@ use crate::parser::ast::ASTNode;
 use crate::parser::lexer::Lexer;
 use std::iter::Peekable;
 
+use super::ast::ParseError;
+use super::token::Token;
+
 pub struct Parser {
     lexer: Peekable<Lexer>,
 }
@@ -12,6 +15,16 @@ impl Parser {
             lexer: Lexer::new(source).peekable(),
         }
     }
+
+    // if there is an error, we will try to recover from it
+    // by skipping the rest of the line
+    fn recover_from_parse_error(&mut self) {
+        while let Some(token) = self.lexer.next() {
+            if token == Token::Newline {
+                break;
+            }
+        }
+    }
 }
 
 impl Iterator for Parser {
@@ -19,6 +32,25 @@ impl Iterator for Parser {
 
     fn next(&mut self) -> Option<Self::Item> {
         // if next item is not a newline and astnode is not a label then error
-        ASTNode::try_from(&mut self.lexer).ok()
+        loop {
+            let mut item = ASTNode::try_from(&mut self.lexer);
+
+            // if item is an ast parse error, then keep trying
+            while let Err(ParseError::IsNewline) = item {
+                item = ASTNode::try_from(&mut self.lexer);
+            }
+
+            return match item {
+                Ok(ast) => Some(ast),
+                Err(err) => match err {
+                    ParseError::UnexpectedEOF => None,
+                    ParseError::IsNewline => todo!(),
+                    _ => {
+                        self.recover_from_parse_error();
+                        continue;
+                    }
+                },
+            };
+        }
     }
 }
