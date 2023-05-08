57732423d5f32df12c436793b0ef0e8a6e600a02
Rajan Maghera <rmaghera@ualberta.ca>
2023-05-08 15:21:52 -0600

    Added NL lexing

--- src/parser/lexer.rs
+++ src/parser/lexer.rs
@@ -55,19 +55,19 @@ impl Lexer {
     }
 
     fn is_space(&self) -> bool {
-        self.ch == ' ' || self.ch == '\t' || self.ch == '\n'
+        self.ch == ' ' || self.ch == '\t'
     }
 
     fn is_symbol_char(&self) -> bool {
         let c = self.ch;
         // TODO be careful, we may not want - to be a symbol character,
         // This is done so number parsing is only done once we know what the instruction is
+        // aka. to make our lives easier
         (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_' || c == '-'
     }
 
     fn is_symbol_item(&self) -> bool {
         let c = self.ch;
-        dbg!(c);
         self.is_symbol_char() || (c >= '0' && c <= '9')
     }
 
@@ -79,159 +79,162 @@ impl Lexer {
 }
 
 // TODO typestate for lexer?
+// TODO extensive support for end of file needed
 
 impl Iterator for Lexer {
     type Item = TokenInfo;
 
     fn next(&mut self) -> Option<Self::Item> {
-                self.skip_ws();
-                dbg!(self.ch);
-                let token = match self.ch {
-                    ':' => Some(TokenInfo {
-                        token: Token::Colon,
-                        pos: Range {
-                            start: Position {
-                                line: self.row,
-                                column: self.col,
-                            },
-                            end: Position {
-                                line: self.row,
-                                column: self.col,
-                            },
-                        },
-                    }),
-                    ',' => Some(TokenInfo {
-                        token: Token::Comma,
-                        pos: Range {
-                            start: Position {
-                                line: self.row,
-                                column: self.col,
-                            },
-                            end: Position {
-                                line: self.row,
-                                column: self.col,
-                            },
-                        },
-                    }),
-                    '.' => {
-                        // directive
-                        
-                        let start = Position {
-                            line: self.row,
-                            column: self.col,
-                        };
-                        self.next_char();
-
-                        let mut dir_str: String = "".to_owned();
-
-                        while self.is_symbol_item() {
-                            dir_str += &self.ch.to_string();
-                            self.next_char();
-                        }
-
-                        dbg!(&dir_str);
-
-                        let end = Position {
-                            line: self.row,
-                            column: self.col,
-                        };
-
-                        if dir_str == "" {
-                            // this is an error or end of line?
-                            return None;
-                        }
-
-                        Some(TokenInfo {
-                            token: Token::Directive(dir_str.to_owned()),
-                            pos: Range { start, end },
-                        })
-
+        self.skip_ws();
+        let token = match self.ch {
+            '\n' => Some(TokenInfo {
+                token: Token::Newline,
+                pos: Range {
+                    start: Position {
+                        line: self.row,
+                        column: self.col,
                     },
-                    '#' => {
-                        // skip line till newline
-                        while self.ch != '\n' {
-                            self.next_char();
-                        }
-                        // TODO eventually we will return a comment token
-                        // for now we just skip it
-                        // Remember that recursive calls are bad
-                        return self.next();
+                    end: Position {
+                        line: self.row,
+                        column: self.col,
                     },
+                },
+            }),
+            '.' => {
+                // directive
 
-                    '"' => {
-                        // string
-                        let start = Position {
-                            line: self.row,
-                            column: self.col,
-                        };
-
-                        let mut string_str: String = "".to_owned();
-
-                        self.next_char();
-
-                        while self.ch != '"' {
-                            string_str += &self.ch.to_string();
-                            self.next_char();
-                        }
-
-                        self.next_char();
-
-                        let end = Position {
-                            line: self.row,
-                            column: self.col,
-                        };
-
-                        Some(TokenInfo {
-                            token: Token::String(string_str.to_owned()),
-                            pos: Range { start, end },
-                        })
-                    }
-                    _ => {
-                        let start = Position {
-                            line: self.row,
-                            column: self.col - 1,
-                        };
-
-                        let mut symbol_str: String = "".to_owned();
-
-                        while self.is_symbol_item() {
-                            symbol_str += &self.ch.to_string();
-                            self.next_char();
-                        }
-
-                        if symbol_str == "" {
-                            // this is an error or end of line?
-                            return None;
-                        } else if self.ch == ':' {
-                            // this is a label
-                            self.next_char();
-                            let end = Position {
-                                line: start.line,
-                                column: self.col,
-                            };
-
-                            return Some(TokenInfo {
-                                token: Token::Label(symbol_str.to_owned()),
-                                pos: Range { start, end },
-                            });
-                        }
-
-                        let end = Position {
-                            line: start.line,
-                            column: self.col,
-                        };
-
-                        Some(TokenInfo {
-                            token: Token::Symbol(SymbolData(symbol_str.to_owned())),
-                            pos: Range { start, end },
-                        })
-                    }
+                let start = Position {
+                    line: self.row,
+                    column: self.col,
                 };
+                self.next_char();
 
-                if let Some(_) = token {
+                let mut dir_str: String = "".to_owned();
+
+                while self.is_symbol_item() {
+                    dir_str += &self.ch.to_string();
                     self.next_char();
                 }
 
-                token
+                dbg!(&dir_str);
+
+                let end = Position {
+                    line: self.row,
+                    column: self.col,
+                };
+
+                if dir_str == "" {
+                    // TODO this is an error or end of line?
+                    return None;
+                }
+
+                Some(TokenInfo {
+                    token: Token::Directive(dir_str.to_owned()),
+                    pos: Range { start, end },
+                })
             }
+            '#' => {
+                // skip line till newline
+                while self.ch != '\n' && self.ch != EOF_CONST {
+                    self.next_char();
+                }
+
+                if self.ch == EOF_CONST {
+                    return None
+                }
+
+                // TODO switch to comment tokens
+                // For now, we return the newline, as it ends
+                // in a newline
+                Some(TokenInfo {
+                    token: Token::Newline,
+                    pos: Range {
+                        start: Position {
+                            line: self.row,
+                            column: self.col,
+                        },
+                        end: Position {
+                            line: self.row,
+                            column: self.col,
+                        },
+                    },
+                })
+            }
+
+            '"' => {
+                // string
+                let start = Position {
+                    line: self.row,
+                    column: self.col,
+                };
+
+                let mut string_str: String = "".to_owned();
+
+                self.next_char();
+
+                while self.ch != '"' {
+                    string_str += &self.ch.to_string();
+                    self.next_char();
+                }
+
+                self.next_char();
+
+                let end = Position {
+                    line: self.row,
+                    column: self.col,
+                };
+
+                Some(TokenInfo {
+                    token: Token::String(string_str.to_owned()),
+                    pos: Range { start, end },
+                })
+            }
+            _ => {
+                let start = Position {
+                    line: self.row,
+                    column: self.col - 1,
+                };
+
+                let mut symbol_str: String = "".to_owned();
+
+                while self.is_symbol_item() {
+                    symbol_str += &self.ch.to_string();
+                    self.next_char();
+                }
+
+                if symbol_str == "" {
+                    // this is an error or end of line?
+                    return None;
+                } else if self.ch == ':' {
+                    // this is a label
+                    self.next_char();
+                    let end = Position {
+                        line: start.line,
+                        column: self.col,
+                    };
+
+                    return Some(TokenInfo {
+                        token: Token::Label(symbol_str.to_owned()),
+                        pos: Range { start, end },
+                    });
+                }
+
+                let end = Position {
+                    line: start.line,
+                    column: self.col,
+                };
+
+                Some(TokenInfo {
+                    token: Token::Symbol(SymbolData(symbol_str.to_owned())),
+                    pos: Range { start, end },
+                })
+            }
+        };
+
+        if let Some(_) = token {
+            self.next_char();
+        }
+
+        token
+    }
 }
