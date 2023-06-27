ebeb23fbb2f08d8ef9d717f09c55d144774c653a
Rajan Maghera <rmaghera@ualberta.ca>
2023-06-27 14:10:00 -0600

    Add display for CFG

--- src/cfg/display.rs
+++ src/cfg/display.rs
@@ -1,3 +1,67 @@
+use std::{
+    collections::{HashMap, HashSet},
+    fmt::Display,
+};
+
+use itertools::Itertools;
+
+use super::{CFGNode, Cfg};
+
+trait SetListString {
+    fn str(&self) -> String;
+}
+
+impl<T> SetListString for HashSet<T>
+where
+    T: Display,
+{
+    fn str(&self) -> String {
+        self.iter()
+            .map(|x| x.to_string())
+            .sorted()
+            .collect::<Vec<_>>()
+            .join(", ")
+    }
+}
+
+impl<T, U> SetListString for HashMap<T, U>
+where
+    T: Display,
+    U: Display,
+{
+    fn str(&self) -> String {
+        self.iter()
+            .map(|(k, v)| format!("[{}: {}]", k, v))
+            .sorted()
+            .collect::<Vec<_>>()
+            .join(", ")
+    }
+}
+
+impl Display for CFGNode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("{}\n", self.node))?;
+        f.write_fmt(format_args!("  | LIVE | {}\n", self.live_out().str()))?;
+        f.write_fmt(format_args!("  | VALS | {}\n", self.reg_values_out().str()))?;
+        f.write_fmt(format_args!(
+            "  | STCK | {}\n",
+            self.stack_values_out().str()
+        ))?;
+        f.write_fmt(format_args!("  | UDEF | {}\n", self.u_def().str()))?;
+
+        Ok(())
+    }
+}
+
+impl Display for Cfg {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for node in &self.nodes {
+            f.write_fmt(format_args!("{}\n", node))?;
+        }
+        Ok(())
+    }
+}
+
 // use std::fmt::Display;
 
 // use super::CFGNode;
