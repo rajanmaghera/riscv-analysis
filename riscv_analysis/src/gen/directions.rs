322ba43c0058c937e55e5bd1fb98d1227392ca5c
nulmer-student <nulmer@ualberta.ca>
2024-09-23 09:06:49 -0600

    Add iterator to missed iteration

--- riscv_analysis/src/gen/directions.rs
+++ riscv_analysis/src/gen/directions.rs
@@ -12,31 +12,30 @@ use crate::{
 pub struct NodeDirectionPass;
 impl GenerationPass for NodeDirectionPass {
     fn run(cfg: &mut Cfg) -> Result<(), Box<CfgError>> {
-        let nodes = &cfg.nodes();
         let mut prev = None;
-        for node in nodes {
+        for node in cfg.iter() {
             // If node jumps to another node, add it to the nexts of the current node and the prevs of the node it jumps to.
             if let Some(label) = node.node().jumps_to() {
-                let jump_to_node = nodes
+                let jump_to_node = cfg
                     .iter()
                     .find(|n| n.labels.contains(&label))
                     .ok_or_else(|| CfgError::UnexpectedError)?;
 
-                node.insert_next(Rc::clone(jump_to_node));
-                jump_to_node.insert_prev(Rc::clone(node));
+                node.insert_next(Rc::clone(&jump_to_node));
+                jump_to_node.insert_prev(Rc::clone(&node));
             }
 
             // Linearly scan for nexts and prevs
             if let Some(prev) = prev {
                 node.insert_prev(Rc::clone(&prev));
-                prev.insert_next(Rc::clone(node));
+                prev.insert_next(Rc::clone(&node));
             }
 
             // Set previous node to current node, if it is not a return
             prev = if node.node().is_return() || node.node().is_unconditional_jump() {
                 None
             } else {
-                Some(Rc::clone(node))
+                Some(Rc::clone(&node))
             }
         }
 
