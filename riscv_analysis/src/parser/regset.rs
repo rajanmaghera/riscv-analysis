01e6b08dbc59455cbab2de569776df799cc3640c
Rajan Maghera <rmaghera@ualberta.ca>
2025-02-25 10:31:45 -0700

    feat: add all set for register set

--- riscv_analysis/src/parser/regset.rs
+++ riscv_analysis/src/parser/regset.rs
@@ -28,6 +28,20 @@ impl RegSets {
     }
 
     #[must_use]
+    pub fn all() -> RegisterSet {
+        use Register::{
+            X1, X10, X11, X12, X13, X14, X15, X16, X17, X18, X19, X2, X20, X21, X22, X23, X24, X25,
+            X26, X27, X28, X29, X3, X30, X31, X4, X5, X6, X7, X8, X9,
+        };
+        [
+            X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, X11, X12, X13, X14, X15, X16, X17, X18, X19,
+            X20, X21, X22, X23, X24, X25, X26, X27, X28, X29, X30, X31,
+        ]
+        .into_iter()
+        .collect()
+    }
+
+    #[must_use]
     pub fn saved() -> RegisterSet {
         use Register::{X18, X19, X20, X21, X22, X23, X24, X25, X26, X27, X8, X9};
         [X8, X9, X18, X19, X20, X21, X22, X23, X24, X25, X26, X27]
