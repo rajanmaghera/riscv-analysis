8dc16d429f3024c98c751977a4fb050dc240a98f
Max Leontiev <mleontie@ualberta.ca>
2025-08-25 15:49:37 -0600

    feat: add all_registers and stack_pointer_registers to CallingConventionISA

--- riscv_analysis/src/new_impl/calling_convention.rs
+++ riscv_analysis/src/new_impl/calling_convention.rs
@@ -3,6 +3,9 @@ use crate::new_impl::isa::ISA;
 use crate::new_impl::limited_element_set::LimitedElementSet;
 
 pub trait CallingConventionISA: ISA {
+    fn all_registers(
+    ) -> <<<Self as ISA>::Instruction as InstructionLike>::Register as LimitedElementSet>::ArrayType;
+
     fn caller_saved_registers(
     ) -> <<<Self as ISA>::Instruction as InstructionLike>::Register as LimitedElementSet>::ArrayType;
 
@@ -13,6 +16,8 @@ pub trait CallingConventionISA: ISA {
     fn return_registers(
     ) -> <<<Self as ISA>::Instruction as InstructionLike>::Register as LimitedElementSet>::ArrayType;
     fn program_argument_registers(
+    ) -> <<<Self as ISA>::Instruction as InstructionLike>::Register as LimitedElementSet>::ArrayType;
+    fn stack_pointer_registers(
     ) -> <<<Self as ISA>::Instruction as InstructionLike>::Register as LimitedElementSet>::ArrayType;
 
     fn is_function_call_instruction(inst: &<Self as ISA>::Instruction) -> bool;
